53996b2462f5f1f9eb989b052992a16ec7342c7f
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-03 23:59:12 +0300

    fix footnote link

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MPC-HC'
-copyright = u'2012, MPC-HC Team'
+copyright = u'MPC-HC Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
