c1ae21e0479c52f8905a66030f282ee1ae9735a9
Gwildor Sok <gwildorsok@gmail.com>
2012-12-12 22:40:13 +0100

    PEP8 fixes.

--- ext/traclinks.py
+++ ext/traclinks.py
@@ -11,19 +11,22 @@ for example:
 creates a link to ticket number 2015.
 
 adapted from recipe here:
-http://stackoverflow.com/questions/2096401/sphinx-generate-automatic-references-to-trac-tickets-and-changesets
+http://stackoverflow.com/questions/2096401/sphinx-generate-automatic-references-to-trac-tickets-and-changesets  # noqa
 '''
 
 import urllib
 from docutils import nodes, utils
 
+
 def make_trac_link(name, rawtext, text, lineno, inliner,
                    options={}, content=[]):
     env = inliner.document.settings.env
-    trac_url =  env.config.traclinks_base_url
+    trac_url = env.config.traclinks_base_url
     ref = trac_url + '/intertrac/' + urllib.quote(text, safe='')
-    node = nodes.reference(rawtext, utils.unescape(text), refuri=ref, **options)
-    return [node],[]
+    node = nodes.reference(rawtext, utils.unescape(text),
+        refuri=ref, **options)
+    return [node], []
+
 
 # setup function to register the extension
 def setup(app):
