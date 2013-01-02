b8be36963bb1d678d2ad6e1ad6f97c18ba5026b1
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-02 12:25:20 +0200

    move google analytics to plugins.js and use vanilla code

--- source/_static/js/plugins.js
+++ source/_static/js/plugins.js
@@ -22,3 +22,16 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-9335012-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
