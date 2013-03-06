e46ea7e9438a3b73409c24b030f0a62fced8034a
XhmikosR <xhmikosr@users.sourceforge.net>
2013-03-06 11:37:00 +0200

    make.js: add JSHint support

--- make.js
+++ make.js
@@ -166,15 +166,39 @@
 
 
     //
+    // make check
+    //
+    target.check = function () {
+        cd(ROOT_DIR);
+        echo();
+        echo('### Running JSHint...');
+
+        var jshintFiles = ['make.js',
+                           'source/_static/js/plugins.js'];
+
+        exec('jshint' + ' ' + jshintFiles.join(' '));
+    };
+
+
+    //
+    // make lint
+    //
+    target.lint = function () {
+        target.check();
+    };
+
+
+    //
     // make help
     //
     target.help = function () {
         echo("Available targets:");
-        echo("  website  builds the website");
-        echo("  server   starts the webserver");
-        echo("  clean    cleans the built website");
-        echo("  rebuild  rebuilds the website");
-        echo("  help     shows this help message");
+        echo("  website       builds the website");
+        echo("  server        starts the webserver");
+        echo("  clean         cleans the built website");
+        echo("  rebuild       rebuilds the website");
+        echo("  check / lint  runs JSHint for our js files");
+        echo("  help          shows this help message");
     };
 
 }());
