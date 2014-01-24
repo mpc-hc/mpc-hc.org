81c14d23ebdb5b9d465dfd5683164246fd04cb7a
XhmikosR <xhmikosr@users.sourceforge.net>
2014-01-24 08:52:03 +0200

    Switch to the new Google universal analytics.

--- source/assets/js/plugins.js
+++ source/assets/js/plugins.js
@@ -31,18 +31,14 @@ $('body.downloads').find('a[href^="http://sourceforge.net/projects/mpc-hc/files/
     $(this).addClass('sourceforge_accelerator_link');
 });
 
+
 // Google analytics
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-9335012-2']);
-_gaq.push(['_gat._anonymizeIp']);
-_gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-9335012-2', 'mpc-hc.org');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
