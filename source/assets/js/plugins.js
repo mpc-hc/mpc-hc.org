fcdc54dfa0c1b202054e6f290386ff636e78e77d
XhmikosR <xhmikosr@gmail.com>
2015-07-17 08:54:05 +0300

    plugins.js: minor refactoring.

--- source/assets/js/plugins.js
+++ source/assets/js/plugins.js
@@ -24,22 +24,26 @@
 // Place any jQuery/helper plugins in here.
 
 
-$('.alert-dismissible').each(function() {
-    var $alert = $(this);
-    var alertNameLSProp = $alert.attr('data-alert-id');
+(function() {
+    $(document).ready(function () {
+        $('.alert-dismissible').each(function() {
+            var $alert = $(this);
+            var $dataAlertId = $alert.attr('data-alert-id');
 
-    if (!window.localStorage) {
-        return;
-    }
+            if (!window.localStorage) {
+                return;
+            }
 
-    if (localStorage.getItem(alertNameLSProp) === 'true') {
-        $alert.remove();
-    } else {
-        $alert.on('close.bs.alert', function() {
-            localStorage.setItem(alertNameLSProp, true);
+            if (localStorage.getItem($dataAlertId) === 'true') {
+                $alert.remove();
+            } else {
+                $alert.on('close.bs.alert', function() {
+                    localStorage.setItem($dataAlertId, true);
+                });
+            }
         });
-    }
-});
+    });
+}());
 
 
 // Google analytics
