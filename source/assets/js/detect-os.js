008f25ad0b2988d22dd968c311399afcf3f3c948
XhmikosR <xhmikosr@gmail.com>
2016-03-09 09:02:18 +0200

    Convert detect-os.js to vanilla JS.
    It should work in IE9+.
    
    # Conflicts:
    #	source/assets/js/detect-os.js

--- source/assets/js/detect-os.js
+++ source/assets/js/detect-os.js
@@ -1,26 +1,29 @@
-/* jshint browser:true, jquery:true */
-
-$(document).ready(function (navigator) {
+/* jshint browser:true */
+(function() {
     "use strict";
 
     var arch = navigator.userAgent.match(/x86_64|Win64|WOW64/) ||
                navigator.cpuClass === "x64" ? "x64" : "x86";
     var href = "https://binaries.mpc-hc.org/";
     var version = "@{mpcVersionShort}@";
-    var element = $("#downloadButton");
-    var buttonSubArchText = $(".button-sub span");
-    var folder;
+    var element = document.getElementById("downloadButton");
 
-    if (arch === "x64") {
-        folder = "MPC%20HomeCinema%20-%20x64";
-        buttonSubArchText.text("64-bit");
-    } else {
-        folder = "MPC%20HomeCinema%20-%20Win32";
-        buttonSubArchText.text("32-bit");
+    // getElementById returns null if it has no matches
+    if (element) {
+        // querySelector is enough here since we only need the first match
+        var buttonSubArchText = document.querySelector(".button-sub span");
+        var folder;
+
+        if (arch === "x64") {
+            folder = "MPC%20HomeCinema%20-%20x64";
+            buttonSubArchText.textContent = "64-bit";
+        } else {
+            folder = "MPC%20HomeCinema%20-%20Win32";
+            buttonSubArchText.textContent = "32-bit";
+        }
+
+        href += folder + "/MPC-HC_v" + version + "_" + arch + "/MPC-HC." + version + "." + arch + ".exe";
+
+        element.setAttribute("href", href);
     }
-
-    href += folder + "/MPC-HC_v" + version + "_" + arch + "/MPC-HC." + version + "." + arch + ".exe";
-
-    element.attr("href", href);
-
-}(navigator));
+})();
