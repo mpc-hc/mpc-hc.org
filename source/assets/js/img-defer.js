9bbaacbab7144fe8e46d78fdc9c3728c323ab4b6
XhmikosR <xhmikosr@gmail.com>
2016-04-11 15:07:49 +0300

    img-defer.js: add a check and cleanup old IE code.

--- source/assets/js/img-defer.js
+++ source/assets/js/img-defer.js
@@ -5,23 +5,20 @@
 (function() {
     "use strict";
 
-    function imgDefer() {
-        var imgElement = document.getElementsByTagName("img");
+    var imgElement = document.getElementsByTagName("img");
 
-        for (var i = 0, len = imgElement.length; i < len; i++) {
-            if (imgElement[i].hasAttribute("data-src")) {
-                imgElement[i].setAttribute("src", imgElement[i].getAttribute("data-src"));
-                imgElement[i].removeAttribute("data-src");
-                imgElement[i].className += " loaded";
+    if (imgElement) {
+
+        var imgDefer = (function () {
+            for (var i = 0, len = imgElement.length; i < len; i++) {
+                if (imgElement[i].hasAttribute("data-src")) {
+                    imgElement[i].setAttribute("src", imgElement[i].getAttribute("data-src"));
+                    imgElement[i].removeAttribute("data-src");
+                    imgElement[i].className += " loaded";
+                }
             }
-        }
-    }
+        });
 
-    if (window.addEventListener) {
         window.addEventListener("load", imgDefer, false);
-    } else if (window.attachEvent) {
-        window.attachEvent("onload", imgDefer);
-    } else {
-        window.onload = imgDefer;
     }
 }());
