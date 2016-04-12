dd764a50ff61b5409021853aeb8d198e9591d3cc
XhmikosR <xhmikosr@gmail.com>
2016-04-12 13:46:40 +0300

    Ignore `WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.Button.Name`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -260,6 +260,9 @@ module.exports = function(grunt) {
         accessibility: {
             options: {
                 accessibilityLevel: "WCAG2AA",
+                ignore: [
+                    "WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.Button.Name"
+                ],
                 reportLevels: {
                     notice: false,
                     warning: false,
