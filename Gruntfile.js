569db5b48a9229d1c0d4ddb4a2459358f1e1b6c6
XhmikosR <xhmikosr@gmail.com>
2020-04-23 15:25:32 +0300

    Update clean-css config

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,14 @@ module.exports = function(grunt) {
                     minifyCSS: {
                         level: {
                             1: {
-                                specialComments: 0
+                                specialComments: 0,
+                                roundingPrecision: 6
+                            },
+                            2: {
+                                all: false,
+                                mergeMedia: true,
+                                removeDuplicateMediaBlocks: true,
+                                removeEmpty: true
                             }
                         }
                     },
@@ -130,7 +137,14 @@ module.exports = function(grunt) {
                 options: {
                     level: {
                         1: {
-                            specialComments: 0
+                            specialComments: 0,
+                            roundingPrecision: 6
+                        },
+                        2: {
+                            all: false,
+                            mergeMedia: true,
+                            removeDuplicateMediaBlocks: true,
+                            removeEmpty: true
                         }
                     }
                 },
